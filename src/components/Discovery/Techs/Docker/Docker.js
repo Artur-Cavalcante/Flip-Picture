fdcda566145e67dcfa576f80cdce8cbc2d38bf6e
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2020-03-18 15:58:54 -0300

    Alter struct of flip and discovery

---
+++ src/components/Discovery/Techs/Docker/Docker.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function docker_top_left() {
+    return ('../images/top_left.png');
+};
+
+export default docker_top_left;
--- src/components/Flip/Flip.js
+++ src/components/Flip/Flip.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import Button from '../Button/Button'
-
+import Button from '../Button/Button';
 
 function Flip(props) {
+
+
     return (
         <section className="">
             <div className="">
-                <img src={props.src_top_left} alt="top_left"/>
-                <img src={props.src_top_right} alt="top_right"/>
-                <img src={props.src_bottom_left} alt="bottom_left"/>
-                <img src={props.src_bottom_right} alt="bottom_right"/>
+
             </div>
                 
             <div className="">
                 <Button name="Left" />
-                <Button name="Direito" />
+                <Button name="Right" />
             </div>
         </section>
     );
